27d2d397b7d6cd4ca62a7e1a4e1738de154fb098
@manuelduarte077 <carlosd99m@gmail.com>
2022-05-31 19:51:15 -0600

    feat: add bloc

---
+++ lib/bloc/article_list_bloc.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+
+import 'package:article_finder/bloc/bloc.dart';
+import 'package:article_finder/data/article.dart';
+import 'package:article_finder/data/rw_client.dart';
+
+class ArticleListBloc implements Bloc {
+  /// This line creates instance of RWClient to communicate with raywenderlich.com based on HTTP protocol.
+  final _client = RWClient();
+
+  /// The code gives a private StreamController declaration. It will manage the input sink for this BLoC. StreamControllers use generics to tell the type system what kind of object the stream will emit.
+  final _searchQueryController = StreamController<String?>();
+
+  /// Sink<String?> is a public sink interface for your input controller _searchQueryController. You’ll use this sink to send events to the BLoC.
+  Sink<String?> get searchQuery => _searchQueryController.sink;
+
+  /// articlesStream stream acts as a bridge between ArticleListScreen and ArticleListBloc. Basically, the BLoC will stream a list of articles onto the screen. You’ll see late syntax here. It means you have to initialize the variable in the future before you first use it. The late keyword helps you avoid making these variables as null type.
+  late Stream<List<Article>?> articlesStream;
+
+  ArticleListBloc() {
+    /// This code processes the input queries sink and build an output stream with a list of articles. asyncMap listens to search queries and uses the RWClient class from the starter project to fetch articles from the API. It pushes an output event to articlesStream when fetchArticles completes with some result.
+    articlesStream = _searchQueryController.stream
+
+        /// 1: startWith(null) produces an empty query to start loading all articles. If the user opens the search for the first time and doesn’t enter any query, they see a list of recent articles.
+        .startWith(null)
+        .debounceTime(const Duration(milliseconds: 500))
+
+        /// 2: debounceTime skips queries that come in intervals of less than 100 milliseconds. When the user enters characters, TextField sends multiple onChanged{} events. debounce skips most of them and returns the last keyword event.
+
+        .switchMap(
+          /// 3: Replace asyncMap with switchMap. These operators are similar, but switchMap allows you to work with other streams.
+          (query) => _client
+              .fetchArticles(query)
+              .asStream() // 4: Convert Future to Stream
+              .startWith(null),
+
+          /// 5: startWith(null) at this line sends a null event to the article output at the start of every fetch request. So when the user completes the search query, UI erases the previous list of articles and shows the widget’s loading. It happens because _buildResults in article_list_screen.dart listens to your stream and displays a loading indicator in the case of null data.
+        );
+  }
+
+  /// Finally, in the cleanup method, you close StreamController. If you don’t do this, the IDE complains that the StreamController is leaking.
+  @override
+  void dispose() {
+    _searchQueryController.close();
+  }
+}
---
+++ lib/bloc/bloc_provider.dart
@@ -0,0 +1,36 @@
+import 'package:article_finder/bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+/// 1: BlocProvider is a generic class. The generic type T is scoped to be an object that implements the Bloc interface. This means the provider can store only BLoC objects.
+class BlocProvider<T extends Bloc> extends StatefulWidget {
+  final Widget child;
+  final T bloc;
+
+  const BlocProvider({
+    Key? key,
+    required this.bloc,
+    required this.child,
+  }) : super(key: key);
+
+  /// 2: The of method allows widgets to retrieve the BlocProvider from a descendant in the widget tree with the current build context. This is a common pattern in Flutter.
+  static T of<T extends Bloc>(BuildContext context) {
+    final BlocProvider<T> provider = context.findAncestorWidgetOfExactType()!;
+    return provider.bloc;
+  }
+
+  @override
+  State createState() => _BlocProviderState();
+}
+
+class _BlocProviderState extends State<BlocProvider> {
+  /// 3: The widget’s build method is a passthrough to the widget’s child. This widget won’t render anything.
+  @override
+  Widget build(BuildContext context) => widget.child;
+
+  /// 4: Finally, the only reason the provider inherits from StatefulWidget is to get access to the dispose method. When this widget is removed from the tree, Flutter calls the dispose method, which in turn closes the stream.
+  @override
+  void dispose() {
+    widget.bloc.dispose();
+    super.dispose();
+  }
+}
